09bd0386849052b162fe25eaa118c59c54d296b0
Compiler Destroyer <alcarinpower@gmail.com>
2026-04-07 14:56:00 +0300

    .

---
+++ Editor/Project Management/EmbedEditorVisualProject.cs
@@ -0,0 +1,96 @@
+using UnityEditor.PackageManager;
+using UnityEditor;
+using UnityEditor.PackageManager.Requests;
+using System.Linq;
+using System.IO;
+
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
+using Debug = UnityEngine.Debug;
+using System.Security.AccessControl;
+
+namespace CodeDestroyer.Editor.EditorVisual
+{
+    public class EmbedEditorVisualProject
+    {
+        private static ListRequest listRequest;
+        private static EmbedRequest Request;
+        private static readonly string thisScriptPath = Path.GetFullPath(GlobalVariables.ProjectManagerPath) + Path.DirectorySeparatorChar + "EmbedEditorVisualProject.cs";
+
+
+        [InitializeOnLoadMethod]
+        private static void InitEmbeddingEditorVisualProject()
+        {
+            if (File.Exists(GlobalVariables.ProjectTempInstalledFilePath)) return;
+
+            PackageInfo packageInfo = PackageInfo.FindForPackageName(GlobalVariables.UnityEditorVisualPackageName);
+            PackageSource packageSource = PackageSource.Unknown;
+            if (packageInfo != null)
+            {
+                packageSource = packageInfo.source;
+            }
+            Debug.Log(packageSource);
+            if (packageSource != PackageSource.Embedded && packageSource != PackageSource.Local && packageSource != PackageSource.LocalTarball)
+            {
+                Debug.Log(GlobalVariables.UnityEditorVisualPackageName + " is embedding now!");
+                TrySearchEmbeddedPackage();
+            }
+            else
+            {
+                File.Delete(thisScriptPath);
+                File.Delete(thisScriptPath + ".meta");
+                AssetDatabase.Refresh();
+            }
+        }
+
+        private static void TrySearchEmbeddedPackage()
+        {
+            listRequest = Client.List();
+            EditorApplication.update += ListProgress;
+        }
+        static void ListProgress()
+        {
+            if (listRequest.IsCompleted)
+            {
+                if (listRequest.Status == StatusCode.Success)
+                {
+
+                    if (listRequest.Result.Any(pkg => pkg.name == GlobalVariables.UnityEditorVisualPackageName))
+                    {
+                        EmbedProject(GlobalVariables.UnityEditorVisualPackageName);
+                    }
+                }
+                else
+                {
+                    Debug.Log(listRequest.Error.message);
+                }
+
+                EditorApplication.update -= ListProgress;
+            }
+        }
+
+        static void EmbedProject(string inTarget)
+        {
+            Request = Client.Embed(inTarget);
+            EditorApplication.update += EmbedProgress;
+        }
+
+        static void EmbedProgress()
+        {
+            if (Request.IsCompleted)
+            {
+                if (Request.Status == StatusCode.Success)
+                {
+                    File.Delete(thisScriptPath);
+                    File.Delete(thisScriptPath + ".meta");
+                    AssetDatabase.Refresh();
+                }
+                else if (Request.Status >= StatusCode.Failure)
+                {
+
+                }
+
+                EditorApplication.update -= EmbedProgress;
+            }
+        }
+    }
+}
